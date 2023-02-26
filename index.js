6deda77fd2593b133a9d668f6cde20998792b8d4
Joseph Garcia <prosif@gmail.com>
2023-02-25 19:05:06 -0700

    reset password stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Cognito = require('amazon-cognito-identity-js');
 const AWS = require('aws-sdk');
- const poolData = {
+const poolData = {
     UserPoolId: process.env.COGNITO_USER_POOL_ID,
     ClientId: process.env.COGNITO_CLIENT_ID
 };
@@ -14,6 +14,116 @@ const getUser = (username) => {
 
     return new Cognito.CognitoUser(userData);
 };
+
+const confirmResetPassword = ({email, password, code}) => new Promise((resolve, reject) => {
+    if (!email || !password || !code) {
+        console.log('missing email, password or code');
+        reject();
+    }
+    console.log('confirming reset for email ' + email);
+    findUserByEmail(email).then(results => {
+        if (results.Users.length > 0) {
+            if (results.Users.length != 1) {
+                console.log(`Somehow found multiple users for email ${email}`);
+                reject();
+            } else {
+                const username = results.Users[0].Username;
+                console.log('found username for reset password ' + username);
+         
+                const params = {
+                    ClientId: process.env.COGNITO_CLIENT_ID,
+                    ConfirmationCode: code,
+                    Password: password,
+                    Username: username
+                };
+                
+                const provider = new AWS.CognitoIdentityServiceProvider({region: process.env.awsRegion});
+            
+                provider.confirmForgotPassword(params, (err, data) => {
+                    console.log('confirm forgot response');
+                    console.log(err);
+                    console.log(data);
+                    if (err) {
+                        reject({success: false});
+                    } else {
+                        resolve({success: true});
+                    }
+                });
+            }
+        }
+    });
+});
+
+const sendResetPasswordRequest = (username) => new Promise((resolve, reject) => {
+    const params = {
+        ClientId: process.env.COGNITO_CLIENT_ID,
+        Username: username
+    };
+
+    const provider = new AWS.CognitoIdentityServiceProvider({region: process.env.awsRegion});
+
+    provider.forgotPassword(params, (err, data) => {
+        console.log('user ' + username + ' forgot password');
+        console.log(err);
+        console.log(data);
+        resolve();
+    });
+});
+
+const resetPassword = (payload) => new Promise((resolve, reject) => {
+    if(!payload.email) {
+        reject();
+    } else {
+        resolve();
+
+        findUserByEmail(payload.email).then((results) => {
+            console.log("found that user by email " + payload.email);
+            if (results.Users.length > 0) {
+                if (results.Users.length != 1) {
+                    console.log(`Somehow found multiple users for email ${payload.email}`);
+                } else {
+                    const username = results.Users[0].Username;
+                    sendResetPasswordRequest(username);
+                }
+            }
+            console.log(user);
+        }).catch(() => {
+        });
+    }
+});
+
+const findUserByUsername = (username) => new Promise((resolve, reject) => {
+    const params = {
+        UserPoolId: process.env.COGNITO_USER_POOL_ID,
+        Filter: `username = "${username}"`
+    };
+
+    const provider = new AWS.CognitoIdentityServiceProvider({region: process.env.awsRegion});
+
+    provider.listUsers(params, (err, data) => {
+        if (err) {
+            reject(err);
+        } 
+
+        resolve(data);
+    });
+
+});
+
+const getCognitoUser = (username) => new Promise((resolve, reject) => {
+    const params = {
+        UserPoolId: poolData.UserPoolId,
+        Username: username
+    };
+
+    const provider = new AWS.CognitoIdentityServiceProvider({region: 'us-west-2'});
+
+    provider.adminGetUser(params, (err, data) => {
+        resolve({
+            created: data.UserCreateDate
+        });
+    });
+});
 
 const login = (payload) => new Promise((resolve, reject) => {
 
@@ -36,10 +146,13 @@ const login = (payload) => new Promise((resolve, reject) => {
 
     user.authenticateUser(authDetails, {
         onSuccess: (result) => {
-            resolve({
-                accessToken: result.getAccessToken().getJwtToken(),
-                idToken: result.getIdToken().getJwtToken(),
-                refreshToken: result.getRefreshToken().getToken()
+            getCognitoUser(username).then((cognitoUser) => {
+                resolve({
+                    created: cognitoUser.created,
+                    accessToken: result.getAccessToken().getJwtToken(),
+                    idToken: result.getIdToken().getJwtToken(),
+                    refreshToken: result.getRefreshToken().getToken()
+                });
             });
         },
         onFailure: (err) => {
@@ -123,7 +236,7 @@ const verify = (payload) => new Promise((resolve, reject) => {
 
 });
 
-const findUser = (email) => new Promise((resolve, reject) => {
+const findUserByEmail = (email) => new Promise((resolve, reject) => {
     const params = {
         UserPoolId: process.env.COGNITO_USER_POOL_ID,
         Filter: `email = "${email}"`
@@ -144,7 +257,7 @@ const signUp = (payload) => new Promise((resolve, reject) => {
     if (!payload.username || !payload.email || !payload.password) {
         reject('Signup requires username, email & password');
     }
-    findUser(payload.email).then(userData => {
+    findUserByEmail(payload.email).then(userData => {
         if (userData.Users && userData.Users.length > 0) {
             reject("User with that email already exists");
         } else {
@@ -197,6 +310,8 @@ const inputHandlers = {
     refresh,
     verify,
     signUp,
+    resetPassword,
+    confirmResetPassword,
     confirmUser
 };
 
